84133a21f61d0d77a9dfd5d992434cf9cc5b86a8
HaabuK <karl.h.haabu@gmail.com>
2023-12-07 15:25:08 +0200

    swagger uuendus

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const app = require('express')()
-const port = 8080
+const port = 7070
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
+
+app.get('/jobs', (req, res) => {
+  res. send([ "Koristaja", "Kokk" ])
+})
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
