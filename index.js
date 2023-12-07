7b1bd314f1c4e2e3ae182d22fa73802c902b50f9
HaabuK <karl.h.haabu@gmail.com>
2023-12-07 15:46:32 +0200

    As an app developer i want to get a list of all jobs to show in my app [Delivers #186636458]

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = 7070
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 
+
 app.get('/jobs', (req, res) => {
   res. send([ "Koristaja", "Kokk" ])
 })
