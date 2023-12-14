0490ccbb1bff82ba032a7224d67d1d2fb25b5270
HaabuK <karl.h.haabu@gmail.com>
2023-12-14 15:17:56 +0200

    [Delivers #186636496]
    As an app developer i want to be able to add a new job

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const app = require('express')()
+//const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 7070
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 const yamljs = require('yamljs')
+
+app.use(express.json())
 
 const jobs = [{id: 1,name: "Koristaja", quote: 6},
 {id: 2,name: "Kokk", quote: 10.00},
@@ -25,8 +29,31 @@ app.get('/jobs/:id', (req, res) => {
     res.send(jobs[req.params.id - 1])
 })
 
+app.post('/jobs', (req, res) => {
+  if (!req.body.name || !req.body.quote) {
+    return res.status(400).send({ error: 'One or all params are missing' })
+  }
+  let job = {
+    id: jobs.length + 1, 
+    quote: req.body.quote,
+    name: req.body.name
+  }
+
+  jobs.push(job)
+  res.status(201)
+  .location(`${getBaseUrl(req)}/jobs/${jobs.length}`)
+  .send(job)
+})
+
+//xh -v http://localhost:7070/jobs name=Kuller quote=8.60
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
   console.log('API up at: http://localhost:${port}')
 })
+
+function getBaseUrl(req) { 
+  return req.connection && req.connection.encrypted
+  ? 'https': 'http' + `://${req.headers.host}`
+}
