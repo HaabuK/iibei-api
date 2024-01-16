b4e133a7f881bfb198b98fc7ded765aaecc98c07
viljar norman <99358447+viljarnorman@users.noreply.github.com>
2024-01-16 12:50:02 +0200

    [Delivers #186826485]
    As a developer i can add workers to the database

--- controllers/workersController.js
+++ controllers/workersController.js
@@ -11,6 +11,27 @@ exports.getById = async (req, res) => {
     res.send(workers)
 }
 
+exports.createNew = async (req, res) => {
+    let worker
+    try {
+        worker = await Worker.create(req.body)
+    } catch (error) {
+        if (error instanceof db.Sequelize.ValidationError) {
+            console.log(error)
+            res.status(400).send({"error":error.errors.map((item)=> item.message)})
+        } else {
+            console.log("WorkersCreate: ", error)
+            res.status(500).send({"error":"Something has gone wrong"})
+        }
+        return
+    }
+    res
+    .status(201)
+    .location(`${getBaseUrl(req)}/workers/${worker.id}`)
+    .json(worker);
+    console.log(worker)
+}
+
 getBaseUrl = (request) => {
     return (
         (request.connection && request.connection.encryption ? "https" : "http") +
